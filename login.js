320c648faa21134aafb1bc282419c11599fa8ece
okeniyiferanmi <122177041+okeniyiferanmi@users.noreply.github.com>
2023-01-07 21:57:37 +0100

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-correctPassword = "iamthehost";
-function checkPassword(){
-  var inputPassword = window.prompt("This is a private website please exit");
-  if(inputPassword == correctPassword){
-    window.open("index.html");
-  }else{
-    alert("This is a private website please exit");
-  }
-}
